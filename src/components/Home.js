01fe8e5c7164413d1ba292d07334a443c05e7e72
liujinmeng <1321839748@qq.com>
2017-04-02 10:56:27 +0800

    material-ui load-css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state={
-
+      cats:[]
     }
+  }
+  componentWillMount(){
+    axios.get('http://tiger.haoduoshipin.com/cats')
+      .then(res => this.setState({cats:res.data.cats}))
   }
   render(){
     return(
-      <div>
-        Home
+      <div className='home'>
+        <p>已发布的课程</p>
+        <div className='home-root'>
+          {this.state.cats.map(item =>
+            <div key={Math.random()} className='card'>
+              <div className='image'></div>
+              <p>{item.name}</p>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class SignIn extends React.Component {
   constructor() {
@@ -7,10 +10,31 @@ class SignIn extends React.Component {
 
     }
   }
+  handleSubmit(){
+
+  }
   render(){
     return(
-      <div>
-        SignIn
+      <div className='signin-root'>
+        <MuiThemeProvider>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <div>
+              <label>用户名</label>
+              <TextField
+                hintText="Please input username"
+                floatingLabelText="username"
+              />
+            </div>
+            <div>
+              <label>密&nbsp;&nbsp;&nbsp;码</label>
+              <TextField
+                hintText="Please input password"
+                floatingLabelText="password"
+              />
+            </div>
+            <RaisedButton className='button' label="登 录" secondary={true} onTouchTap={this.handleSubmit.bind(this)}/>
+          </form>
+        </MuiThemeProvider>
       </div>
     )
   }
