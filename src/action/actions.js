d301807c1acd7a7ab57f6b9e8f42e7af4d2d9fd6
liujinmeng <1321839748@qq.com>
2017-04-03 10:13:35 +0800

    localStorage

--- src/action/actions.js
+++ src/action/actions.js
@@ -13,8 +13,11 @@ export function userInfo(username,password) {
   return dispatch => {
     return axios.post('http://tiger.haoduoshipin.com/user/signin',{username,password})
       .then(res => {
+        console.log(res)
         dispatch({type:'SIGNIN',userData:res.data})
         browserHistory.push('/');
+        localStorage.setItem('user',res.data.user)
+        localStorage.setItem('userId',res.data.userId)
       }
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,17 +13,28 @@ class Header extends React.Component {
   }
   handleLogout(){
     axios.get('http://tiger.haoduoshipin.com/user/logout')
-      .then(res =>
+      .then(res => {
         store.dispatch({type:'SIGNIN',userData:res.data})
-      )
+        localStorage.user = '';
+        localStorage.userId = '';
+      }
+    )
+  }
+  componentWillMount(){
+    let data = {
+      msg:"登陆成功",
+      user:`${localStorage.getItem('user')}`,
+      userId:`${localStorage.getItem('userId')}`
+    }
+    store.dispatch({type:'SIGNIN',userData:data})
   }
   render(){
     return(
       <div className='header-root'>
         <Link to='/'>首页</Link>
         <div className='right'>
-          {this.props.userData.msg==='登陆成功' ? <a>{this.props.userData.user}</a> : <Link to='/signin' activeClassName='cools'>登录</Link>}
-          {this.props.userData.msg==='登陆成功' ? <a onClick={this.handleLogout.bind(this)}>退出</a> : <Link to='/signup' activeClassName='cools'>注册</Link>}
+          {this.props.userData.user ? <a>{this.props.userData.user}</a> : <Link to='/signin' activeClassName='cools'>登录</Link>}
+          {this.props.userData.user ? <a onClick={this.handleLogout.bind(this)}>退出</a> : <Link to='/signup' activeClassName='cools'>注册</Link>}
         </div>
       </div>
     )
